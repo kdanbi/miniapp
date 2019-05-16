285ee216a00f6eb85752e53720aa85ae9354e967
Danbi Kim <danbikim@Danbis-Air.localdomain>
2019-05-16 15:46:36 -0400

    main and app set up

---
+++ mini-app/src/components/Result.js
@@ -0,0 +1,15 @@
+//import App from '../App';
+import React from 'react';
+import Places from "google-places-web";
+Places.APIkeY = "AIzaSyC2DtGQafS7ey_uIJHawxlOx1QrsGF55qs";
+//Places.debug = __DEV__; //boolean;
+
+class Result extends React.Component {
+    render () {
+        return (
+            'hello'
+        )
+    }
+}
+
+export default Result;
