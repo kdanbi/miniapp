9c006e585dccb61752a0dc37948c7e44f4e577fd
Frank Zhang <frankzhang@TheArchitect.localdomain>
2019-05-17 15:30:33 -0400

    axios set up

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const axios = require('axios');
 
 const app = express(); 
+app.use(express.json());
 
 app.listen(8080, console.log('server listening on 8080'));
 
@@ -10,5 +11,5 @@ app.use(cors());
 
 app.get('/', (req, res) => {
     axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=43.645665, -79.395369&rankby=distance&type=restaurant&key=AIzaSyC2DtGQafS7ey_uIJHawxlOx1QrsGF55qs')
-        .then(result => res.json(result))
+        .then(result => res.json((result.data.results)))
 })
