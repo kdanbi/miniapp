9ee050cf244efa7bf2f3de7d3faf4009175de748
frank zhang <frankbusinessmail@gmail.com>
2019-05-21 13:46:06 -0400

    minor changes

--- client/mini-app/src/components/Result.js
+++ client/mini-app/src/components/Result.js
@@ -6,6 +6,26 @@ const Result = (props) => {
 
     return(
         <>  
+        <fieldset class="radiogroup"> 
+            <legend>Price Level</legend> 
+            <ul class="radio"> 
+                <li><input type="radio" name="crust" id="crust1" value="deep" /><label for="crust1">Deep dish</label></li> 
+                <li><input type="radio" name="crust" id="crust2" value="thick" /><label for="crust2">Thick</label></li> 
+                <li><input type="radio" name="crust" id="crust3" value="hand" /><label for="crust3">Hand thrown</label></li> 
+                <li><input type="radio" name="crust" id="crust4" value="thin" /><label for="crust4">Thin</label></li> 
+            </ul> 
+        </fieldset> 
+
+        <fieldset class="radiogroup"> 
+            <legend>Rating</legend> 
+            <ul class="radio"> 
+                <li><input type="radio" name="crust" id="crust1" value="deep" /><label for="crust1">Deep dish</label></li> 
+                <li><input type="radio" name="crust" id="crust2" value="thick" /><label for="crust2">Thick</label></li> 
+                <li><input type="radio" name="crust" id="crust3" value="hand" /><label for="crust3">Hand thrown</label></li> 
+                <li><input type="radio" name="crust" id="crust4" value="thin" /><label for="crust4">Thin</label></li> 
+            </ul> 
+        </fieldset> 
+
             {result.map(item=>{
                 return(
                     <>
