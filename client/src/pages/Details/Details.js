833fea1f247e5f89b1c5cb1e985a8a4e571e95c5
Danbi Kim <danbikim@danbis-air.lan>
2019-07-18 15:53:20 -0400

    title change

--- client/src/pages/Details/Details.js
+++ client/src/pages/Details/Details.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import {Link} from 'react-router-dom';
+//import {Link} from 'react-router-dom';
 import DetailsMap from "../../components/DetailsMap/DetailsMap";
 import './Details.scss';
  
